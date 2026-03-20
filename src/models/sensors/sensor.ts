4e33e53139ea6d0de4a59d03a552451e9f68f327
boukhalfa <boukhalfakcahtel229@gmail.com>
2026-03-20 08:39:33 -0700

    Update abstract class Sensor for statistical calculation

--- src/models/sensors/sensor.ts
+++ src/models/sensors/sensor.ts
@@ -13,11 +13,12 @@ export abstract class Sensor{
         this.history = rawHistory.map(h => new SensorHistory(h.timestamp, h.value));
     }
 
-    
+    //Ajoute une nouvelle mesure
     addRecord(timestamp: string, value: number | Position){
         this.history.push(new SensorHistory(timestamp, value));
     }
 
+    // Retourne tout l'historique 
     getHistory() {
         return this.history;
     }
@@ -30,5 +31,39 @@ export abstract class Sensor{
     // Récupère la dernière valeur enregistrée
     getLastValue() {
         return this.history.length > 0 ? this.history[this.history.length - 1].value : null;
+    }
+
+    // Renvoie QUE des nombres pour les calculs
+    getNumericValues(): number[] {
+        return this.history
+            .map(h => h.value)
+            .filter((v): v is number => typeof v === 'number');
+    }
+
+    // Calcule la moyenne des valeurs numeriques de l'historique
+    getAverage(): number {
+
+        const values = this.getNumericValues();
+        if(values.length === 0 ) return 0;
+
+        const sum = values.reduce((acc, val) => acc + val, 0);
+        return sum / values.length;
+    }    
+
+    //Retourne le max
+    getMaxValue() : number{
+        const values = this.getNumericValues();
+        if(values.length === 0) return 0;
+        return Math.max(...values);
+    }
+    
+    //Calcul de l'évolution (différence entre première et dernière valeur)
+    getEvolution(): number{
+        
+        const values = this.getNumericValues();
+
+        if(values.length < 2  ) return 0;
+ 
+        return values[values.length -1] - values[0];
     }
 }
