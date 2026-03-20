42aa50ff06fd81b5f49f04edbea0048345133012
boukhalfa <boukhalfakcahtel229@gmail.com>
2026-03-20 06:50:50 -0700

    Update abstract class Vehicle

--- src/models/vehicles/vehicle.ts
+++ src/models/vehicles/vehicle.ts
@@ -19,17 +19,19 @@ export abstract class Vehicle {
         return this.sensors;
     }
 
-    //Ajoute un capteur 
-    addSensor(sensor:Sensor): void{
-        const exists = this.sensors.find(s => s.id === sensor.id);
-        if(!exists){
-            this.sensors.push(sensor);
+    //Ajoute un nouveau capteur ou remplace l'ancien 
+    addAndReplaceSensor(sensor:Sensor): void{
+        const index = this.sensors.findIndex(s => s.id === sensor.id);
+        if(index !== -1){
+            this.sensors[index] = sensor;
         }
+        this.sensors.push(sensor);
     }
 
     //Supprime un capteur par son ID
     removeSensor(sensorId: number): void{
         this.sensors = this.sensors.filter(s => s.id !== sensorId);
-        
+
     }
+
 }
