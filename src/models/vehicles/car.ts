6e9d2181f403840d55b1772a08edfb491047d348
boukhalfa <boukhalfakcahtel229@gmail.com>
2026-03-20 06:20:16 -0700

    Add class Car

---
+++ src/models/vehicles/car.ts
@@ -0,0 +1,26 @@
+import { Vehicle } from './vehicle';
+import { Sensor } from '../sensors/sensor';
+import { FuelLevelSensor } from '../sensors/fuelLevelSensor';
+
+export class Car extends Vehicle{
+    constructor(
+        id: number,
+        brand: string,
+        model: string,
+        year: number,
+        public fuelType: string,
+        sensors: Sensor[] = []
+    ) {
+        super(id, brand, model, year, sensors);
+    }
+
+    //Retourne le niveau de carburant
+    getFuelLevel(): number {
+        const sensor = this.getSensor('Fuel');
+        if(sensor instanceof FuelLevelSensor){
+            return sensor.getFuelLevel();
+        }
+        return 0;
+    }
+
+}
